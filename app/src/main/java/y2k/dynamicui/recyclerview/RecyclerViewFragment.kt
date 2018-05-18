ea228cc9784cba23100d2f403d41be9ae7ed8263
y2k <itwisterlx@gmail.com>
2018-05-18 11:32:16 +0300

    add recyclerview example

--- app/src/main/java/y2k/dynamicui/recyclerview/RecyclerViewFragment.kt
+++ app/src/main/java/y2k/dynamicui/recyclerview/RecyclerViewFragment.kt
@@ -4,16 +4,21 @@ import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.recyclerview.extensions.ListAdapter
 import android.support.v7.util.DiffUtil
-import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.fragment_recyclerview.*
+import kotlinx.android.synthetic.main.item_1.*
 import y2k.dynamicui.R
 
 class RecyclerViewFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        list.adapter = Adapter()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
@@ -25,7 +30,7 @@ class EditItem : Item()
 
 private class Adapter : ListAdapter<Item, Adapter.VH>(itemCallback) {
 
-    private val items = List(5, { EditItem() })
+    private val items = List(15, { EditItem() })
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH =
         VH(LayoutInflater.from(parent.context).inflate(R.layout.item_1, parent, false))
@@ -34,8 +39,11 @@ private class Adapter : ListAdapter<Item, Adapter.VH>(itemCallback) {
 
     override fun onBindViewHolder(holder: VH, position: Int) = holder.bind(items[position])
 
-    private class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(item: Item): Unit = TODO()
+    private class VH(override val containerView: View) : ViewHolder(containerView), LayoutContainer {
+
+        fun bind(item: Item) {
+            title.text = "${item.hashCode()}"
+        }
     }
 
     companion object {
