f141ba8d6e41daa2249df2cc35a57e6a59df52f9
y2k <itwisterlx@gmail.com>
2018-06-08 23:32:09 +0300

    refactoring

--- fsharp/src/Home.fs
+++ fsharp/src/Home.fs
@@ -2,8 +2,9 @@ module Home
 
 open System
 open Model
-open Fable.Helpers.ReactNative
 open Elmish
+open Fable.Helpers.ReactNative
+open Fable.Helpers.ReactNative.Props
 
 module Cmd =
     open Elmish
@@ -14,10 +15,14 @@ module Effects =
     open Fable.PowerPack
     let loadItems = 
         promise {
+            do! Promise.sleep 500
             let rand = Random()
             return
-                [0 .. rand.Next(10)]
-                |> List.map (fun i -> SwitchItem { title = sprintf "One #%i" i; isChecked = true; id = rand.Next() })
+                [0 .. rand.Next(4, 10)]
+                |> List.map (fun i -> 
+                    match rand.Next(2) % 2 with
+                    | 0 -> SwitchItem { title = sprintf "Switch #%i" i; isChecked = true; id = rand.Next() }
+                    | _ -> NumberItem { value = rand.Next(); id = rand.Next() })
         }
 
 type Msg =
@@ -31,28 +36,41 @@ type Model = { configs : Item list; loading : Boolean }
 
 let update (msg : Msg) model : Model * Cmd<Msg> =
     match msg with
-    | Reload -> model, Cmd.ofEffect Effects.loadItems Loaded
-    | Loaded (Ok configs) -> { model with configs = configs }, Cmd.none
+    | Reload -> 
+        { model with loading = true; configs = [] }, Cmd.ofEffect Effects.loadItems Loaded
+    | Loaded (Ok configs) -> 
+        { model with configs = configs; loading = false }, Cmd.none
+    | Switch item -> 
+        { model with configs = Configs.changeSwitch model.configs item.id }, Cmd.none
+    | Click (item, increase) -> 
+        { model with configs = Configs.changeNumber model.configs item.id increase }, Cmd.none
+    | SeekBar (item, value) -> 
+        { model with configs = Configs.changeSeekBar model.configs item.id value }, Cmd.none
     | Loaded (Error _) -> model, Cmd.none
-    | Switch (item) -> model, Cmd.none
-    | Click (item, increase) -> model, Cmd.none
-    | SeekBar (item, value) -> model, Cmd.none
 
 let init () = { configs = []; loading = false }, Cmd.ofMsg (Reload)
 
 let rec configToView dispatch item  =
     match item with
     | GroupItem (_, _, _, children) ->
-        view [] (children |> List.map (configToView dispatch))
+        children |> List.map (configToView dispatch) |> view []
     | SwitchItem x -> 
         switch [] (fun _ -> dispatch <| Switch x) x.isChecked
     | SeekBarItem _ -> 
         view [] [] // slider [ SliderProperties.OnValueChange ( (Func<_,_>) (fun _ -> ())) ]
-    | NumberItem _ -> 
-        view [] []
+    | NumberItem x -> 
+        view [ ViewProperties.Style [ FlexStyle.FlexDirection FlexDirection.Row ] ] 
+             [ Styles.button "-" (fun () -> dispatch <| Click (x, false))
+               text [ TextProperties.Style [ FlexStyle.Flex 1.
+                                             TextStyle.TextAlign TextAlignment.Center ] ] 
+                    (sprintf "%o" x.value)
+               Styles.button "+" (fun () -> dispatch <| Click (x, true))
+             ]
 
 let view (model : Model) (dispatch : Msg -> unit) =
     view [ Styles.sceneBackground ]
-         [ Styles.button "Reload" (fun () -> dispatch Reload) 
+         [ button [ ButtonProperties.Title (if model.loading then "Loading..." else "Reload")
+                    ButtonProperties.OnPress (fun () -> dispatch Reload)
+                    ButtonProperties.Disabled model.loading ] [ ]
            (model.configs |> List.map (configToView dispatch)) |> view []
          ]
--- fsharp/src/Model.fs
+++ fsharp/src/Model.fs
@@ -3,8 +3,8 @@ module Model
 open System
 
 type SwitchItem = { title : String; isChecked : Boolean; id : Int32 }
-type SeekBarItem = SeekBarItem of value : Single * id : Int32
-type NumberItem = NumberItem of value : Int32 * id : Int32
+type SeekBarItem = { value : Single; id : Int32 }
+type NumberItem = { value : Int32; id : Int32 }
 
 type Item =
 | GroupItem of title : String * isEnabled : Boolean * id : Int32 * children : Item list
@@ -14,21 +14,10 @@ type Item =
 
 module Configs =
     let changeNumber configs id increase : Item list = configs
-    let changeSwitch configs id : Item list = configs
+    let changeSwitch configs id : Item list = 
+        configs
+        |> List.map (fun x -> function
+                              | SwitchItem x -> SwitchItem { x with isChecked = not x.isChecked }
+                              | x -> x)
     let changeSeekBar configs id value : Item list = configs
     let flatConfigs configs : Item list = configs
-
-type LocationId = string
-
-[<RequireQualifiedAccess>]
-type LocationStatus =
-| Ok
-| Alarm of string
-
-type LocationCheckRequest = {
-    LocationId : LocationId
-    Name: string
-    Address: string
-    Status : LocationStatus option
-    Date : DateTime option
-}
